73a505abe64fdca4e4f842969c5f1cbca323c3de
kiku123 <kiku18is@cmrit.ac.in>
2023-12-11 16:30:17 +0530

    Fixed Region -us

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mubin-devops-cicd-terraform-eks"
+    bucket = "mubin-devops-cicd-terraform-eks-1"
     region = "us-east-2"
     key = "eks/terraform.tfstate"
   }
